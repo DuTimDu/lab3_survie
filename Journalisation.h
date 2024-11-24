5f60448c16770b31cc10b964168c3539c77c01bb
Timothé DURAND <timothedurand2004@gmail.com>
2024-11-23 23:11:57 -0500

    update survie.txt

--- Journalisation.h
+++ Journalisation.h
@@ -9,9 +9,9 @@ class Journalisation
 {
 public:
 	template <typename T>
-	static void sauvegarder(T* obj)
+	static void sauvegarder(const T* obj)
 	{
 	ofstream monFichier("survie.txt", std::ios::app);
-	monFichier << typeid(*obj).name() << endl;
+	monFichier << typeid(obj).name() << endl;
 	}
 };
